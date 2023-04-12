738146d01bd25eb1caed5e872b68a82628226f12
rubz1e <rubchenya.work@gmail.com>
2023-04-13 00:28:38 +0300

    fix: Corrected folder path#5

--- src/page/RoutingError/RoutingError.tsx
+++ src/page/RoutingError/RoutingError.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { notFounds } from './RoutingError.styled';
 
-import RoutingErrors from './../../assets/notfound.svg';
+import RoutingErrors from '../../assets/notfound.svg';
 
 export default function RoutingError() {
   return (
