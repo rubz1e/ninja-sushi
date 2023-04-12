faa0a86ffe752c5409416199e410c653db90295a
rubz1e <rubchenya.work@gmail.com>
2023-04-13 00:24:20 +0300

    feat: added svg in RoutingError

--- src/page/RoutingError/RoutingError.tsx
+++ src/page/RoutingError/RoutingError.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { notFounds } from './RoutingError.styled';
 
+import RoutingErrors from './../../assets/notfound.svg';
+
 export default function RoutingError() {
   return (
     <notFounds.errorContainer>
       <notFounds.error>
+        <RoutingErrors />
         <notFounds.h2>Ошибка 404.</notFounds.h2>
         <notFounds.h3>Что-то пошло не так.</notFounds.h3>
         <notFounds.p>Страница, которую вы ищете, недействительна или находится на этапе разработки.</notFounds.p>
